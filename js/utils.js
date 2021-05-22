efc6add56168ac7ea7d46ed9f347555db72dccc2
gril520 <1599552627@qq.com>
2021-05-22 17:39:09 +0800

    Site updated: 2021-05-22 17:39:06

--- js/utils.js
+++ js/utils.js
@@ -1,167 +1,198 @@
-(function ($) {
-  "use strict";
+/* global Fluid, CONFIG */
 
-  ZHAOO.utils = {
-    debounce: function (func, wait, immediate) {
-      var timeout;
-      return function () {
-        var context = this;
-        var args = arguments;
-        timeout && clearTimeout(timeout);
-        if (immediate) {
-          var callNow = !timeout;
-          timeout = setTimeout(function () {
-            timeout = null;
-          }, wait);
-          if (callNow) func.apply(context, args);
-        } else {
-          timeout = setTimeout(function () {
-            func.apply(context, args);
-          }, wait);
+window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame;
+
+Fluid.utils = {
+
+  listenScroll: function(callback) {
+    var dbc = new Debouncer(callback);
+    window.addEventListener('scroll', dbc, false);
+    dbc.handleEvent();
+    return dbc;
+  },
+
+  unlistenScroll: function(callback) {
+    window.removeEventListener('scroll', callback);
+  },
+
+  scrollToElement: function(target, offset) {
+    var of = $(target).offset();
+    if (of) {
+      $('html,body').animate({
+        scrollTop: of.top + (offset || 0),
+        easing   : 'swing'
+      });
+    }
+  },
+
+  elementInViewport: function(element, heightFactor) {
+    heightFactor = heightFactor || 1;
+    var rect = element.getBoundingClientRect();
+    var height = window.innerHeight || document.documentElement.clientHeight;
+    var top = rect.top;
+    return (top >= 0 && top <= height * (heightFactor + 1))
+      || (top <= 0 && top >= -(height * heightFactor) - rect.height);
+  },
+
+  waitElementVisible: function(selectorsOrElement, callback, heightFactor) {
+    var runningOnBrowser = typeof window !== 'undefined';
+    var isBot = (runningOnBrowser && !('onscroll' in window)) || (typeof navigator !== 'undefined'
+        && /(gle|ing|ro|msn)bot|crawl|spider|yand|duckgo/i.test(navigator.userAgent));
+    var supportsIntersectionObserver = 'IntersectionObserver' in window;
+
+    if (!runningOnBrowser || isBot) {
+      callback();
+      return;
+    }
+
+    var target;
+    if (typeof selectorsOrElement === 'string') {
+      target = document.querySelector(selectorsOrElement);
+    } else {
+      target = selectorsOrElement;
+    }
+
+    var _heightFactor = heightFactor || 2;
+
+    if (Fluid.utils.elementInViewport(target, _heightFactor)) {
+      callback();
+      return;
+    }
+
+    if (supportsIntersectionObserver) {
+      var io = new IntersectionObserver(function(entries, ob) {
+        if (entries[0].isIntersecting) {
+          callback();
+          ob.disconnect();
         }
-      }
-    },
-    throttle: function (func, wait, options) {
-      var timeout, context, args;
-      var previous = 0;
-      if (!options) options = {};
-      var later = function () {
-        previous = options.leading === false ? 0 : new Date().getTime();
-        timeout = null;
-        func.apply(context, args);
-        if (!timeout) context = args = null;
-      }
-      var throttled = function () {
-        var now = new Date().getTime();
-        if (!previous && options.leading === false) previous = now;
-        var remaining = wait - (now - previous);
-        context = this;
-        args = arguments;
-        if (remaining <= 0 || remaining > wait) {
-          if (timeout) {
-            clearTimeout(timeout);
-            timeout = null;
+      }, {
+        threshold : [0],
+        rootMargin: (window.innerHeight || document.documentElement.clientHeight) + 'px'
+      });
+      io.observe(target);
+    } else {
+      var warpCallback = Fluid.utils.listenScroll(function() {
+        if (Fluid.utils.elementInViewport(target, _heightFactor)) {
+          Fluid.utils.unlistenScroll(warpCallback);
+          callback();
+        }
+      });
+    }
+  },
+
+  waitElementLoaded: function(targetId, callback) {
+    var runningOnBrowser = typeof window !== 'undefined';
+    var isBot = (runningOnBrowser && !('onscroll' in window)) || (typeof navigator !== 'undefined'
+    && /(gle|ing|ro|msn)bot|crawl|spider|yand|duckgo/i.test(navigator.userAgent));
+
+    if (!runningOnBrowser || isBot) {
+      callback();
+      return;
+    }
+
+    if ('MutationObserver' in window) {
+      var mo = new MutationObserver(function(records, ob) {
+        var ele = document.getElementById(targetId);
+        if (ele) {
+          callback();
+          ob.disconnect();
+        }
+      });
+      mo.observe(document, { childList: true, subtree: true });
+    } else {
+      document.addEventListener('DOMContentLoaded', function() {
+        callback();
+      });
+    }
+  },
+
+  createScript: function(url, onload) {
+    var s = document.createElement('script');
+    s.setAttribute('src', url);
+    s.setAttribute('type', 'text/javascript');
+    s.setAttribute('charset', 'UTF-8');
+    s.async = false;
+    if (typeof onload === 'function') {
+      if (window.attachEvent) {
+        s.onreadystatechange = function() {
+          var e = s.readyState;
+          if (e === 'loaded' || e === 'complete') {
+            s.onreadystatechange = null;
+            onload();
           }
-          previous = now;
-          func.apply(context, args);
-          if (!timeout) context = args = null;
-        } else if (!timeout && options.trailing !== false) {
-          timeout = setTimeout(later, remaining);
-        }
-      }
-      return throttled;
-    },
-    hasMobileUA: function () {
-      var nav = window.navigator;
-      var ua = nav.userAgent;
-      var pa = /iPad|iPhone|Android|Opera Mini|BlackBerry|webOS|UCWEB|Blazer|PSP|IEMobile|Symbian/g;
-      return pa.test(ua);
-    },
-    isTablet: function () {
-      return (
-        window.screen.width > 767 &&
-        window.screen.width < 992 &&
-        this.hasMobileUA()
-      );
-    },
-    isMobile: function () {
-      return window.screen.width < 767 && this.hasMobileUA();
-    },
-    isDesktop: function () {
-      return !this.isTablet() && !this.isMobile();
-    },
-    isDuringDate: function (beginDateStr, endDateStr) {
-      var curDate = new Date(),
-        beginDate = new Date(beginDateStr),
-        endDate = new Date(endDateStr);
-      if (curDate >= beginDate && curDate <= endDate) {
-        return true;
-      }
-      return false;
-    },
-    bindKeyup: function (code, fn) {
-      $(document).keyup(function (e) {
-        var key = e.which || e.keyCode;;
-        if (key == code) {
-          fn();
-        }
-      });
-    }
-  }
-
-  ZHAOO.zui = {
-    message: function ({ text, type, delay }) {
-      var message = '<div class="zui-message ' + (type || "info") + '"><p>' + text + '</p></div>';
-      $("body").append(message);
-      var e = $(".zui-message");
-      e.ready(function () {
-        e.addClass("in");
-        setTimeout(function () {
-          e.removeClass("in");
-          e.on("transitionend webkitTransitionEnd", function () {
-            $(this).remove();
-          });
-        }, delay || 3000);
-      });
-    },
-    notification: function ({ title, content, type, delay }) {
-      var storage = JSON.parse(localStorage.getItem("notification-closed"));
-      if (storage && storage.indexOf(title) >= 0) return;
-      var notification = '<div class="zui-notification ' + (type || "info") + '"><span>' + title + '</span><p>' + content + '</p><i class="j-notification-close iconfont iconbaseline-close-px"></i></div>';
-      $("body").append(notification);
-      var e = $(".zui-notification");
-      var close = $(".j-notification-close");
-      e.ready(function () {
-        e.addClass("in");
-        setTimeout(function () {
-          e.removeClass("in");
-          e.on("transitionend webkitTransitionEnd", function () {
-            $(this).remove();
-          });
-        }, delay || 3000);
-        close.on("click", function () {
-          e.removeClass("in");
-          if (storage) {
-            (storage.indexOf(title) < 0) && localStorage.setItem("notification-closed", JSON.stringify(storage.concat(title)));
-          } else {
-            localStorage.setItem("notification-closed", JSON.stringify([title]));
-          }
-        });
-      });
-    }
-  }
-
-})(jQuery);
-
-class AsyncLimit {
-  constructor(limit) {
-    this.limit = Number(limit) || 2;
-    this.pool = [];
-    this.current = 0;
-  }
-
-  async run(fn) {
-    if (!fn || typeof fn !== 'function') {
-      throw new Error('Function error.');
-    }
-    if (this.current >= this.limit) {
-      await new Promise(resolve => this.pool.push(resolve));
-    }
-    return this._handleRun(fn);
-  }
-
-  async _handleRun(fn) {
-    this.current++;
-    try {
-      return await fn();
-    } catch (err) {
-      return Promise.reject(err);
-    } finally {
-      this.current--;
-      if (this.pool.length) {
-        this.pool[0]();
-        this.pool.shift();
+        };
+      } else {
+        s.onload = onload;
       }
     }
+    var e = document.getElementsByTagName('script')[0]
+    || document.getElementsByTagName('head')[0]
+    || document.head || document.documentElement;
+    e.parentNode.insertBefore(s, e);
+  },
+
+  createCssLink: function(url) {
+    var l = document.createElement('link');
+    l.setAttribute('rel', 'stylesheet');
+    l.setAttribute('type', 'text/css');
+    l.setAttribute('href', url);
+    var e = document.getElementsByTagName('link')[0]
+    || document.getElementsByTagName('head')[0]
+    || document.head || document.documentElement;
+    e.parentNode.insertBefore(l, e);
+  },
+
+  loadComments: function(selectors, loadFunc) {
+    var ele = document.querySelector('#comments[lazyload]');
+    if (ele) {
+      var callback = function() {
+        loadFunc();
+        ele.removeAttribute('lazyload');
+      };
+      Fluid.utils.waitElementVisible(selectors, callback, CONFIG.lazyload.offset_factor);
+    } else {
+      loadFunc();
+    }
   }
+
+};
+
+/**
+ * Handles debouncing of events via requestAnimationFrame
+ * @see http://www.html5rocks.com/en/tutorials/speed/animations/
+ * @param {Function} callback The callback to handle whichever event
+ */
+function Debouncer(callback) {
+  this.callback = callback;
+  this.ticking = false;
 }
+Debouncer.prototype = {
+  constructor: Debouncer,
+
+  /**
+   * dispatches the event to the supplied callback
+   * @private
+   */
+  update: function() {
+    this.callback && this.callback();
+    this.ticking = false;
+  },
+
+  /**
+   * ensures events don't get stacked
+   * @private
+   */
+  requestTick: function() {
+    if (!this.ticking) {
+      requestAnimationFrame(this.rafCallback || (this.rafCallback = this.update.bind(this)));
+      this.ticking = true;
+    }
+  },
+
+  /**
+   * Attach this as the event listeners
+   */
+  handleEvent: function() {
+    this.requestTick();
+  }
+};
